3a7bc49f40e25bd00af7ebd6ed2b4e414bb06976
bittencourt-lucas <lucascafieiro@hotmail.com>
2019-11-19 20:13:50 -0300

    Week 9 finished, added video, audio and canvas

--- week-nine/scripts/weeknine.js
+++ week-nine/scripts/weeknine.js
@@ -2,7 +2,6 @@
 * Designing, Defining, and Triggering CSS3 Transforms without
 * Custom Libraries (Thought Library)
 ******************************************************************************/
-
 var size = 1;
 
 var increaseSize = function() {
@@ -11,6 +10,47 @@ var increaseSize = function() {
 }
 
 var decreaseSize = function() {
+  // If it turns negative, the image will be inverted, as in any other
+  // scaling function
   size = size - 1;
   document.getElementById("resize").style.transform = 'scale(' + size + ')';
+}
+
+/******************************************************************************
+* HTML5 Tags - Video, Audio, and Canvas
+******************************************************************************/
+var canvas, ctx;
+var counter = 1;
+
+window.onload = function() {
+    document.getElementById("myCanvas").addEventListener("click",
+      changeColor = () => {
+        canvas = document.getElementById("myCanvas");
+        ctx = canvas.getContext("2d");
+        // Create red gradient
+        if (counter == 1) {
+          var grd = ctx.createLinearGradient(0, 0, 200, 0);
+          grd.addColorStop(0, "red");
+          grd.addColorStop(1, "green");
+          counter++;
+        }
+        // Create green gradient
+        else if (counter == 2) {
+          var grd = ctx.createLinearGradient(0, 0, 200, 0);
+          grd.addColorStop(0, "green");
+          grd.addColorStop(1, "blue");
+          counter++;
+        }
+        // Create blue gradient
+        else if (counter == 3) {
+          var grd = ctx.createLinearGradient(0, 0, 200, 0);
+          grd.addColorStop(0, "blue");
+          grd.addColorStop(1, "red");
+          counter = 1;
+        }
+
+        // Fill with gradient
+        ctx.fillStyle = grd;
+        ctx.fillRect(0, 0, 240, 297);
+    })
 }
