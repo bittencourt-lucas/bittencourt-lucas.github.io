cdbba59bd772cebab472d8612d22345456ffaf35
bittencourt-lucas <lucascafieiro@hotmail.com>
2019-11-19 09:00:38 -0300

    Started working on the Video player

--- week-nine/scripts/weeknine.js
+++ week-nine/scripts/weeknine.js
@@ -1,6 +1,16 @@
+/******************************************************************************
+* Designing, Defining, and Triggering CSS3 Transforms without
+* Custom Libraries (Thought Library)
+******************************************************************************/
+
 var size = 1;
 
 var increaseSize = function() {
   size = size + 1;
-  document.getElementById("resize").style.scale = 10;
+  document.getElementById("resize").style.transform = 'scale(' + size + ')';
+}
+
+var decreaseSize = function() {
+  size = size - 1;
+  document.getElementById("resize").style.transform = 'scale(' + size + ')';
 }
