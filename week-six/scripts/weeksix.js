8bd47f7ef84ab6ebff876d492e2f66a8bd09bf23
bittencourt-lucas <lucascafieiro@hotmail.com>
2019-11-22 10:34:44 -0300

    Local Storage added

--- week-six/scripts/weeksix.js
+++ week-six/scripts/weeksix.js
@@ -2,3 +2,26 @@
  * Local Storage API, Storing and Retrieving Simple Data, Arrays,
  * Associative Arrays, and Objects
  *****************************************************************************/
+var store = () => {
+  if (typeof(Storage) !== undefined) {
+    localStorage.setItem("user", "lbittencourt");
+    localStorage.setItem("data", {
+      token: "1",
+      roles: ["admin", "manager"]
+    })
+    localStorage.setItem("associative", {
+      "usersOnline": 1,
+    })
+  } else {
+    console.log("Your browser doesn't support Local Storage! :(")
+  }
+}
+
+var retrieve = () => {
+  if (localStorage.user !== undefined) {
+    document.getElementById("hello-message").innerHTML = "Hello "
+      + localStorage.user;
+  } else {
+    document.getElementById("hello-message").innerHTML = "Hello Stranger";
+  }
+}
