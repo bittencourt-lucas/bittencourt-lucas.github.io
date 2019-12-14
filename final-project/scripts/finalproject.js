117ffba639a4a29de03d85f9f1819a8e809b608b
bittencourt-lucas <lucascafieiro@hotmail.com>
2019-12-14 08:50:37 -0300

    Testing the API

---
+++ final-project/scripts/finalproject.js
@@ -0,0 +1,37 @@
+console.log('Client side JS is loaded')
+
+const getWeather = () => {
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "https://api.darksky.net/forecast/aefe3d980fec4447cc130acb2827083c/-7.118669,-34.848297", true);
+  xhttp.withCredentials = true
+  xhttp.onreadystatechange = function() {
+      if (xhttp.readyState === 4 && xhttp.status === 200) {
+          var obj = JSON.parse(xhttp.responseText);
+          console.log(obj)
+      }
+  };
+  xhttp.setRequestHeader('Content-Type', 'application/json');
+  xhttp.send();
+}
+
+weatherForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const location = search.value
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    fetch('/weather?address=' + encodeURIComponent(search.value)).then(res => {
+        res.json().then(data => {
+            if (data.error) messageOne.textContent = data.error
+            else {
+                messageOne.textContent = data.location
+                messageTwo.textContent =
+                    data.summary +
+                    ' The temperature is ' +
+                    data.temperature +
+                    'F, and there is ' +
+                    data.precipProbability +
+                    '% chance of raininig.'
+            }
+        })
+    })
+})
