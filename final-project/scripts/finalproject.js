b22c3596203974b32acdfd2318f98c48690ada18
bittencourt-lucas <lucascafieiro@hotmail.com>
2019-12-17 23:37:28 -0300

    APIs working on cascade

--- final-project/scripts/finalproject.js
+++ final-project/scripts/finalproject.js
@@ -1,16 +1,47 @@
 console.log('Client side JS is loaded')
 
-const getWeather = () => {
-  var xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/aefe3d980fec4447cc130acb2827083c/-7.118669,-34.848297", true);
-  xhttp.withCredentials = false;
-  xhttp.onreadystatechange = function() {
-      if (xhttp.readyState === 4 && xhttp.status === 200) {
-          var obj = JSON.parse(xhttp.responseText);
-          console.log(obj)
-      }
-  };
-  xhttp.send();
+function geocode(address, callback) {
+    const url = 'https://cors-anywhere.herokuapp.com/https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibGNhZmllaXIiLCJhIjoiY2p5c3dtb2dhMDBnNTNlcG5iNzRhanFpNyJ9.5cuLfCK53msUaKGdlZDMqw&limit=1'
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url);
+    xhttp.withCredentials = false;
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState === 4 && xhttp.status === 200) {
+            let body = JSON.parse(xhttp.responseText);
+            if (body.features.length !== 0) {
+                callback({
+                    latitude: body.features[0].center[1],
+                    longitude: body.features[0].center[0],
+                    location: body.features[0].place_name
+                });
+            } else {
+                console.log('Unable to find location. Try another search.');
+            }
+        } else if (xhttp.status !== 200) {
+            console.log('Unable to connect to location services!');
+        }
+    };
+    xhttp.send();
+}
+
+function getWeather(latitude, longitude, callback) {
+    var xhttp = new XMLHttpRequest();
+    let url = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/aefe3d980fec4447cc130acb2827083c/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude)
+    xhttp.open("GET", url);
+    xhttp.withCredentials = false;
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState === 4 && xhttp.status === 200) {
+            var obj = JSON.parse(xhttp.responseText);
+            console.log(obj)
+        }
+    };
+    xhttp.send();
+}
+
+function wrapper() {
+    geocode('Alaska', ({ location, latitude, longitude } = {}) => {
+        getWeather(latitude, longitude)
+    })
 }
 
 weatherForm.addEventListener('submit', e => {
