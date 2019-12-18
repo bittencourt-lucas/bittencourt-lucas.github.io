1acae30636f88a16d7ef84e60d9fcfebd6f931b7
bittencourt-lucas <lucascafieiro@hotmail.com>
2019-12-18 18:13:04 -0300

    Finished wrapping up everything, animation added

--- final-project/scripts/finalproject.js
+++ final-project/scripts/finalproject.js
@@ -1,7 +1,31 @@
-console.log('Client side JS is loaded')
+function loading() {
+    document.getElementById('loading').style.content = ' ';
+    document.getElementById('loading').style.display = 'block';
+    document.getElementById('loading').style.width = '50px';
+    document.getElementById('loading').style.height = '50px';
+    document.getElementById('loading').style.margin = '8px';
+    document.getElementById('loading').style.borderRadius = '50%';
+    document.getElementById('loading').style.border = '3px solid #888888';
+    document.getElementById('loading').style.borderColor = '#888888 transparent';
+    document.getElementById('loading').style.animation = 'loading-animation 1.2s linear infinite';
+    document.getElementById('loading').style.webkitAnimation = 'loading-animation 1.2s linear infinite';
+}
+
+function loadingFinished() {
+    document.getElementById('loading').style.content = ' ';
+    document.getElementById('loading').style.display = 'block';
+    document.getElementById('loading').style.width = '50px';
+    document.getElementById('loading').style.height = '50px';
+    document.getElementById('loading').style.margin = '8px';
+    document.getElementById('loading').style.borderRadius = '';
+    document.getElementById('loading').style.border = '';
+    document.getElementById('loading').style.borderColor = '';
+    document.getElementById('loading').style.animation = '';
+    document.getElementById('loading').style.webkitAnimation = '';
+}
 
 function geocode(address, callback) {
-    const url = 'https://cors-anywhere.herokuapp.com/https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibGNhZmllaXIiLCJhIjoiY2p5c3dtb2dhMDBnNTNlcG5iNzRhanFpNyJ9.5cuLfCK53msUaKGdlZDMqw&limit=1'
+    const url = 'https://cors-anywhere.herokuapp.com/https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibGJpdHRlbmNvdXJ0IiwiYSI6ImNqc2MybzJpNzBnZHo0NGp4bXVxaGYyZTYifQ.i9iG9fz-t7e9W0Yu4r_QhQ&limit=1'
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", url);
     xhttp.withCredentials = false;
@@ -12,56 +36,44 @@ function geocode(address, callback) {
                 callback({
                     latitude: body.features[0].center[1],
                     longitude: body.features[0].center[0],
-                    location: body.features[0].place_name
+                    place: body.features[0].place_name
                 });
             } else {
-                console.log('Unable to find location. Try another search.');
+                let messageOne = document.getElementById('message-1');
+                messageOne.innerHTML = 'Unable to find location. Try another search.';
             }
         } else if (xhttp.status !== 200) {
-            console.log('Unable to connect to location services!');
+            let messageOne = document.getElementById('message-1');
+            messageOne.innerHTML = 'Unable to connect to location services!';
         }
     };
     xhttp.send();
 }
 
-function getWeather(latitude, longitude, callback) {
+function getWeather(location, latitude, longitude) {
     var xhttp = new XMLHttpRequest();
     let url = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/aefe3d980fec4447cc130acb2827083c/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude)
     xhttp.open("GET", url);
     xhttp.withCredentials = false;
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState === 4 && xhttp.status === 200) {
-            var obj = JSON.parse(xhttp.responseText);
-            console.log(obj)
+            var body = JSON.parse(xhttp.responseText);
+            let messageOne = document.getElementById('message-1');
+            let messageTwo = document.getElementById('message-2');
+            loadingFinished();
+            messageOne.innerHTML = `${location}`;
+            messageTwo.innerHTML = `${body.daily.data[0].summary} 
+            The temperature is ${body.currently.temperature}F, and there is 
+            ${body.currently.precipProbability * 100.0}% chance of raining.`;
         }
     };
     xhttp.send();
 }
 
 function wrapper() {
-    geocode('Alaska', ({ location, latitude, longitude } = {}) => {
-        getWeather(latitude, longitude)
+    loading();
+    const location = document.getElementById("location").value;
+    geocode(location, ({ latitude, longitude, place } = {}) => {
+        getWeather(place, latitude, longitude);
     })
 }
-
-weatherForm.addEventListener('submit', e => {
-    e.preventDefault()
-    const location = search.value
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
-    fetch('/weather?address=' + encodeURIComponent(search.value)).then(res => {
-        res.json().then(data => {
-            if (data.error) messageOne.textContent = data.error
-            else {
-                messageOne.textContent = data.location
-                messageTwo.textContent =
-                    data.summary +
-                    ' The temperature is ' +
-                    data.temperature +
-                    'F, and there is ' +
-                    data.precipProbability +
-                    '% chance of raininig.'
-            }
-        })
-    })
-})
